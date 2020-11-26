6b22b4abcdc07cde791c77c21fb05ee435612ea0
Gasper <duanpengyuan@qq.com>
2020-11-26 16:06:15 +0800

    update layout

--- config/routes.rb
+++ config/routes.rb
@@ -6,29 +6,31 @@ Rails.application.routes.draw do
   post   "login",  to: "sessions#create"
   delete "logout", to: "sessions#destroy"
 
-  resources :folders, only: [:show, :edit, :update, :destroy] do
-    member do
-      get  "make"
-      post "make",   action: :perform_make
-      get  "move"
-      post "move",   action: :perform_move
-      get  "upload"
-      post "upload", action: :perform_upload
+  scope "disk" do
+    resources :folders, only: [:show, :edit, :update, :destroy] do
+      member do
+        get  "make"
+        post "make",   action: :perform_make
+        get  "move"
+        post "move",   action: :perform_move
+        get  "upload"
+        post "upload", action: :perform_upload
+      end
     end
-  end
 
-  resources :uploads, only: [] do
-    member do
-      post "part"
+    resources :uploads, only: [] do
+      member do
+        post "part"
+      end
     end
-  end
 
-  resources :meta_files, only: [:show, :edit, :update, :destroy] do
-    member do
-      get  "move"
-      post "move",   action: :perform_move
-      get  "preview"
-      get  "download"
+    resources :meta_files, only: [:show, :edit, :update, :destroy] do
+      member do
+        get  "move"
+        post "move",   action: :perform_move
+        get  "preview"
+        get  "download"
+      end
     end
   end
 
